cd5ebf357ae295b6159b93e4ad360e010871054c
qiaotian <qiaotian@outlook.com>
2016-02-10 20:24:28 +0800

    update OJ

--- leetcode/392_is_subsequence.cpp
+++ leetcode/392_is_subsequence.cpp
@@ -37,3 +37,25 @@ public:
         return true;
     }
 };
+
+// Official method is to use DP and Binary Search
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        //build a record to store the index of each char in t
+        vector<vector<int>> record(26);
+        //add indexs
+        for(int i = 0; i < t.size(); i++) {
+            record[t[i]-'a'].push_back(i);
+        }
+        //check if each char in s is in the legal place
+        int index = -1;
+        for(int i = 0; i < s.size(); i++) {
+			// find the first index that greater than "index"
+            auto iter = upper_bound(record[s[i]-'a'].begin(), record[s[i]-'a'].end(), index);
+            if(iter == record[s[i]-'a'].end()) return false;
+            index = *iter;
+        }
+        return true;
+    }
+};
