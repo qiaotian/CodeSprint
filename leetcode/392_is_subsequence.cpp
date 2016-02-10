24b381e4d3e96371f640c8c3225a6507580615a9
qiaotian <qiaotian@outlook.com>
2016-02-10 20:26:17 +0800

    update OJ

--- leetcode/392_is_subsequence.cpp
+++ leetcode/392_is_subsequence.cpp
@@ -44,7 +44,7 @@ public:
     bool isSubsequence(string s, string t) {
         //build a record to store the index of each char in t
         vector<vector<int>> record(26);
-        //add indexs
+        //add positions for all 26 characters
         for(int i = 0; i < t.size(); i++) {
             record[t[i]-'a'].push_back(i);
         }
