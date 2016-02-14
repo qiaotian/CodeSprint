7cdbad9d8d665218e8edcbf56d947d4f6432a1cb
qiaotian <qiaotian@outlook.com>
2016-02-15 02:43:48 +0800

    update

--- interviewbit/math_rearrange_array.cpp
+++ interviewbit/math_rearrange_array.cpp
@@ -11,7 +11,7 @@ Lets say N = size of the array. Then, following holds true:
 
 */
 
-
+// 有溢出
 void Solution::arrange(vector<int> &A) {
     // Do not write main() function.
     // Do not read input, instead use the arguments to the function.
@@ -26,3 +26,21 @@ void Solution::arrange(vector<int> &A) {
         A[i] = A[i]>>16;
     }
 }
+
+
+// 参考答案
+// 与上一种方法的位移区别是base选择不同
+// 上一种选择2^16，而当前选择的是N
+class Solution {
+    public:
+    void arrange(vector<int> &Vec) {
+        int N = Vec.size();
+        for(int i = 0; i < N; ++i) {
+            Vec[i] = Vec[i] + (Vec[Vec[i]]%N) * N;
+        }
+
+        for(int i = 0; i < N; ++i) {
+            Vec[i] = Vec[i] / N;
+        }
+    }
+};
