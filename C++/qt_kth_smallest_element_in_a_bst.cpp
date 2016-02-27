b55355810a7abaca27961dc364134728c73bdc16
qiaotian <qiaotian@outlook.com>
2016-02-28 00:31:31 +0800

    update leetcode

--- C++/qt_kth_smallest_element_in_a_bst.cpp
+++ C++/qt_kth_smallest_element_in_a_bst.cpp
@@ -7,6 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// 1st solution (good but not concise)
+/*
 class Solution {
 private:
     vector<TreeNode*> stk; // stack
@@ -35,9 +37,21 @@ public:
         return res;
     }
 };
+*/
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int& k) { // changed the parameter
+        if (root) {
+            int x = kthSmallest(root->left, k);
+            return !k ? x : !--k ? root->val : kthSmallest(root->right, k);
+        }
+    }
+};
 
 /**
  * WARNING
+ * 1st solution
  * 1. right: helper(TreeNode* root, int& k, int& res)
  * 2. wrong: helper(TreeNode* root, int  k, int  res)
  * 
@@ -45,4 +59,12 @@ public:
  * but stored in another address, therefore the k didn't
  * change outside the fuction helper, so does 'res'.
  * 
+ * 2nd solution
+ * We can changed k from int to int& to avoid adding a helper function.
+ * LeetCode doesn't care about that.
+ * 
+ * The main idea of the second solution is visit all left subtree before root and
+ * then visit the right substree if necessary.
+ * 
+ * 递归规则的定义导致解决方案的相当差异！！！
  * /
