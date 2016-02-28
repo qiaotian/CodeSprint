969a848e7aea8eeaa93e6a0b55227f52c8f6bce6
qiaotian <qiaotian@outlook.com>
2016-02-28 09:38:48 +0800

    update leetcode

--- C++/qt_unique_paths.cpp
+++ C++/qt_unique_paths.cpp
@@ -1,6 +1,4 @@
-// TLE for large numbers
-/*
-class Solution {
+ass Solution {
 public:
     int uniquePaths(int m, int n) {
         if(n == 1 || m == 1) return 1;
@@ -26,7 +24,19 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         double res = 1.0;
-        for(int i = 1; i <= n-1; i++) res *= (m+i-1)/i;
+        for(int i = 1; i <= n-1; i++) 
+            res *= (double)(m+i-1)/i;
+        return round(res)
+    }
+};
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        double res = 1.0;
+        for(int i = 1; i <= n-1; i++) 
+            res = res * (m+i-1)/i; // right
+            //res *= (double)(m+i-1)/i; wrong
         return (int)res;
     }
 };
@@ -35,4 +45,7 @@ public:
  * WARNING
  * 1. 当m+i-1和i都是整数时，其比值亦为整数，造成精度损失
  * 2. 强制类型转换的优先级高于四则运算，仅次于[]、取值、和负运算符‘-’
+ * 3. return (int)res will give wrong answer if input is 10 and 10, where output is 48619 but expeected 48620
+ * 4. a *= b*c 和 a = a*b*c的区别在于前者先计算b*c，而后者先计算a*b
  * /
+
