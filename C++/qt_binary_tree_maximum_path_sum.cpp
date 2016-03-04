a00dc8ba168753e4eb4a3740f6bdc590551981b9
qiaotian <qiaotian@outlook.com>
2016-03-05 04:25:32 +0800

    update

--- C++/qt_binary_tree_maximum_path_sum.cpp
+++ C++/qt_binary_tree_maximum_path_sum.cpp
@@ -7,6 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
+// 1st solution
 class Solution {
     int util(TreeNode* root, int& res) {
         // 以root为树，经过root的最大和，root的父节点不考虑
@@ -46,7 +48,29 @@ public:
     }
 };
 
+// 2nd solution
+class Solution {
+    int maxToRoot(TreeNode *root, int &re) {
+        if (!root) return 0;
+        int l = maxToRoot(root->left, re);
+        int r = maxToRoot(root->right, re);
+        if (l < 0) l = 0;
+        if (r < 0) r = 0;
+        if (l + r + root->val > re) re = l + r + root->val;
+        return root->val += max(l, r);
+    }
+public:
+    int maxPathSum(TreeNode *root) {
+        int max = -2147483648;
+        maxToRoot(root, max);
+        return max;
+    }
+};
+
 /**
+ * WARNING
  * 1. 递归计算“当前节点”到其所有叶子节点的最大和min
  * 2. 更新通过“当前节点”的所有路径的最大和res
+ * 3. 两种算法的时间复杂度一致，只是第二种更为简洁
+ * 
  * /
