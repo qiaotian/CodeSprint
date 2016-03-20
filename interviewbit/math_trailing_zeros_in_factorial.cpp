db3acaff7aea8f18eedfb5e5f8c6ac1b20392580
qiaotian <qiaotian@outlook.com>
2016-03-20 10:27:59 +0800

    update

---
+++ interviewbit/math_trailing_zeros_in_factorial.cpp
@@ -0,0 +1,28 @@
+/*
+
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity.
+
+Example :
+
+n = 5
+n! = 120 
+Number of trailing zeros = 1
+So, return 1
+
+*/
+
+
+int Solution::trailingZeroes(int A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    int cnt = 0;
+    while(A) {
+        cnt+=A/5;
+        A/=5;
+    }
+    return cnt;
+}
