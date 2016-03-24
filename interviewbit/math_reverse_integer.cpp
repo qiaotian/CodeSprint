ce3c208c83de561855bdb19dcef1a3372052d7d9
qiaotian <qiaotian@outlook.com>
2016-03-24 11:27:48 +0800

    update

--- interviewbit/math_reverse_integer.cpp
+++ interviewbit/math_reverse_integer.cpp
@@ -35,3 +35,29 @@ int Solution::reverse(int A) {
     
     return isPositive?ans:-1*ans;
 }
+
+// 参考答案
+// To check for overflow/underflow, we could check if ret > 214748364 or ret < –214748364 before multiplying by 10
+
+class Solution {
+public:
+    int reverse(int x) {
+        // solution without using strings
+        int rev = 0, sign = 1, digit;
+        if (x < 0) {
+            sign = -1;
+            x *= -1;
+        }
+        while (x > 0) {
+            digit = x%10;
+            // check for overflow here 
+            if (rev > (INT_MAX / 10) || (rev == (INT_MAX / 10) && digit > (INT_MAX % 10))) {
+                return 0;
+            }
+            rev = rev * 10 + digit;
+            x/=10;
+        }
+        rev *= sign;        
+        return rev;
+    }
+};
