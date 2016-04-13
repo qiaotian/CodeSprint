be81353144e80d3ad2a8bcb567ab10c48194ff6a
qiaotian <qiaotian@outlook.com>
2016-04-13 22:21:21 +0800

    update

--- leetcode/qt_linked_list_node_random_node.cpp
+++ leetcode/qt_linked_list_node_random_node.cpp
@@ -13,7 +13,7 @@ private:
     vector<ListNode*> vec;
 public:
     // @param head The linked list's head.
-    //    Note that the head is guaranteed to be not null, so it contains at least one node. */
+    //    Note that the head is guaranteed to be not null, so it contains at least one node.
     Solution(ListNode* head) {
         ListNode* tmp = head;
         while(tmp) {
