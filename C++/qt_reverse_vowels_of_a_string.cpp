cf3516425c614e38211f564dbd0e9916f625b186
qiaotian <qiaotian@outlook.com>
2016-04-24 16:26:54 +0800

    update leetcode

---
+++ C++/qt_reverse_vowels_of_a_string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int dict[256] = {0};
+        dict['a'] = 1; dict['A'] = 1;
+        dict['e'] = 1; dict['E'] = 1;
+        dict['i'] = 1; dict['I'] = 1;
+        dict['o'] = 1; dict['O'] = 1;
+        dict['u'] = 1; dict['U'] = 1;
+
+        int start = 0; int end = s.size()-1;
+        while(start < end) {
+            while(start < end && !dict[s[start]]) start++;
+            while(start < end && !dict[s[end]]) end--;
+            swap(s[start++], s[end--]);
+        }
+        return s;
+    }
+};
