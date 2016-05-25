19cb2aff825c09cced7d012717db1eb43a6ad85c
qiaotian <qiaotian@outlook.com>
2016-05-25 14:41:38 +0800

    updates codility

---
+++ codility/distinct.cpp
@@ -0,0 +1,18 @@
+int soluton(vector<int>& A) {
+    unordered_set<int, bool> hash;
+    for(auto i:A) {
+        if(hash.find(i) == hash.end()) hash.insert(i);
+    }
+    return hash.size();
+}
+
+// find多余了
+// 直接插入
+
+int soluton(vector<int>& A) {
+    unordered_set<int, bool> hash;
+    for(auto i:A) {
+        hash.insert(i);
+    }
+    return hash.size();
+}
