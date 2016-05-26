2db15c8cf81e6c2f7051fa7df787bf31ed51bc5e
qiaotian <qiaotian@outlook.com>
2016-05-26 14:46:36 +0800

    updates codility

--- codility/max_product_of_three.cpp
+++ codility/max_product_of_three.cpp
@@ -6,4 +6,6 @@
 
 int solution(vector<int> &A) {
     // write your code in C++11 (g++ 4.8.2)
+    sort(A.begin(), A.end());
+    return max(A[A.size()-1]*A[A.size()-2]*A[A.size()-3], A[0]*A[1]*A[A.size()-1]);
 }
