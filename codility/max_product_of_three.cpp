784daae4451163b7aa9aa29b0dab8dfd27db6640
qiaotian <qiaotian@outlook.com>
2016-05-26 13:48:54 +0800

    updates

---
+++ codility/max_product_of_three.cpp
@@ -0,0 +1,9 @@
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++11 (g++ 4.8.2)
+}
