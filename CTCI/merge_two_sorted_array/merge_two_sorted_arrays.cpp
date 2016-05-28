5aa78fe55e49839e6993325d7e7289ede37dd67f
qiaotian <qiaotian@outlook.com>
2016-05-28 21:33:14 +0800

    update

--- CTCI/merge_two_sorted_array/merge_two_sorted_arrays.cpp
+++ CTCI/merge_two_sorted_array/merge_two_sorted_arrays.cpp
@@ -1,5 +1,5 @@
 void merge_two_sorted_arrays(int* A, int* B, int m, int n) {
-    int i;
+    int i= 0;
     for(i = m+n-1; i>=0 && m>=0 && n>=0; i--) {
         A[i] = A[m-1]>B[n-1]?A[(m--)-1]:B[(n--)-1];
     }
