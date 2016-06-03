e0a1473be19522add3175991f445a9bba6dd9eac
qiaotian <qiaotian@outlook.com>
2016-06-03 11:15:14 +0800

    updates

---
+++ CTCI/search_in_2d_sorted_array.cpp
@@ -0,0 +1,10 @@
+/**
+Given a matrix in which each row and each column is sorted, write a method to find whether an element is in it.
+
+Greedy method doesn't work
+*/
+
+bool search_in_2d_sorted_array(vector<vector>& mat, int taget) {
+    
+    return false;
+}
