d0295e1c440d51c9d3764554fc3b57249b487b8b
qiaotian <qiaotian@outlook.com>
2016-07-01 10:35:53 +0800

    leetcode

---
+++ leetcode/qt_plus_one_linked_list.cpp
@@ -0,0 +1,58 @@
+/**
+* @Author: Tian Qiao <qiaotian>
+* @Date:   2016-07-01T08:42:04+08:00
+* @Email:  qiaotian@me.com
+* @Last modified by:   qiaotian
+* @Last modified time: 2016-07-01T10:35:38+08:00
+* @License: Free License
+*/
+
+
+
+/********
+Given a non-negative number represented as a singly linked list of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+Example:
+Input:
+1->2->3
+
+Output:
+1->2->4
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* plusOne(ListNode* head) {
+        //if(!head) return NULL;
+        ListNode *h = new ListNode(0);
+        h->next = head;
+
+        ListNode *not9 = h; // 最后一位非9位
+        ListNode *tmp = head;
+        while(tmp) {
+            if(tmp->val != 9) not9 = tmp;
+            tmp = tmp->next;
+        }
+
+        not9->val++;
+        while(not9->next) {
+            not9->next->val=0;
+            not9 = not9->next;
+        }
+
+        if(h->val==1) head = h;
+        else delete h;
+
+        return head;
+    }
+};
