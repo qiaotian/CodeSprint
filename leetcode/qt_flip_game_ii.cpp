534639eb7342d19403ee579279fd685cfcc66ead
qiaotian <qiaotian@outlook.com>
2016-07-05 08:22:42 +0800

    updates

--- leetcode/qt_flip_game_ii.cpp
+++ leetcode/qt_flip_game_ii.cpp
@@ -3,7 +3,7 @@
 * @Date:   2016-07-04T18:44:03+08:00
 * @Email:  qiaotian@me.com
 * @Last modified by:   Tian Qiao
-* @Last modified time: 2016-07-05T08:13:33+08:00
+* @Last modified time: 2016-07-05T08:22:26+08:00
 * @Inc: Google
 * @Difficulty: Medium
 * @Tag: Backtracking
@@ -27,17 +27,15 @@ class Solution {
 public:
     bool canWin(string s) {
         if(s.size() == 0) return false;
-        bool isMoved = false;
-        for(size_t i = 0; i < s.size() - 1; ++i) {
+        for(int i=0; i < s.size()-1; ++i) {
             if(s[i] == '+' && s[i + 1] == '+') {
-                isMoved = true;
                 s[i] = '-';
-                s[i + 1] = '-';
+                s[i+1] = '-';
                 if(!canWin(s)) return true;
                 s[i] = '+';
-                s[i + 1] = '+';
+                s[i+1] = '+';
             }
         }
-        if(!isMoved) return false;
+        return false;
     }
 };
