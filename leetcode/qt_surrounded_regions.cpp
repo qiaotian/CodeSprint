ae90df4d76925aa585bd4d0c39f7183821feccd1
qiaotian <qiaotian@outlook.com>
2016-07-12 09:30:10 +0800

    updates the leetcode

--- leetcode/qt_surrounded_regions.cpp
+++ leetcode/qt_surrounded_regions.cpp
@@ -3,7 +3,7 @@
 * @Date:   2016-07-11T20:58:51+08:00
 * @Email:  qiaotian@me.com
 * @Last modified by:   root
-* @Last modified time: 2016-07-12T00:26:03+08:00
+* @Last modified time: 2016-07-12T09:29:55+08:00
 */
 
 /*
@@ -30,6 +30,8 @@ X O X X
 
 using namespace std;
 
+// debug耗时太久2h
+// beats 8%
 class Solution {
 public:
     vector<pair<int, int>> directions = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
@@ -81,6 +83,51 @@ public:
             }
         }
     }
+};
+
+// DFS beats 70%
+// optimization
+class Solution {
+public:
+	void solve(vector<vector<char>>& board) {
+        int i,j;
+        int row=board.size();
+        if(!row)
+        	return;
+        int col=board[0].size();
+
+		for(i=0;i<row;i++){
+			check(board,i,0,row,col);
+			if(col>1)
+				check(board,i,col-1,row,col);
+		}
+		for(j=1;j+1<col;j++){
+			check(board,0,j,row,col);
+			if(row>1)
+				check(board,row-1,j,row,col);
+		}
+		for(i=0;i<row;i++)
+			for(j=0;j<col;j++)
+				if(board[i][j]=='O')
+					board[i][j]='X';
+		for(i=0;i<row;i++)
+			for(j=0;j<col;j++)
+				if(board[i][j]=='1')
+					board[i][j]='O';
+    }
+	void check(vector<vector<char> >&vec,int i,int j,int row,int col){
+		if(vec[i][j]=='O'){
+			vec[i][j]='1';
+			if(i>1)
+				check(vec,i-1,j,row,col);
+			if(j>1)
+				check(vec,i,j-1,row,col);
+			if(i+1<row)
+				check(vec,i+1,j,row,col);
+			if(j+1<col)
+				check(vec,i,j+1,row,col);
+		}
+	}
 };
 
 
