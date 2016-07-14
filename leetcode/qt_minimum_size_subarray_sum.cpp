9554e900c8de14864427473fd4708d1319ae0fb9
qiaotian <qiaotian@outlook.com>
2016-07-14 23:44:13 +0800

    update leetcode

--- leetcode/qt_minimum_size_subarray_sum.cpp
+++ leetcode/qt_minimum_size_subarray_sum.cpp
@@ -73,6 +73,9 @@ public:
 };
 
 // O(N)
+/*
+The problem statement has stated that there are both O(n) and O(nlogn) solutions to this problem. Let's see the O(n) solution first (taken from this link), which is pretty clever and short.
+*/
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
@@ -81,14 +84,19 @@ public:
             sum += nums[i];
             while (sum >= s) {
                 minlen = min(minlen, i - start + 1);
-                sum -= nums[start++]; 
+                sum -= nums[start++];
             }
         }
         return minlen == INT_MAX ? 0 : minlen;
     }
 };
 
-// O(N*logN)
+// O(N)
+/*
+Well, you may wonder how can it be O(n) since it contains an inner while loop. Well, the key is that the while loop executes at most once for each starting position start. Then start is increased by 1 and the while loop moves to the next element. Thus the inner while loop runs at most O(n) times during the whole for loop from 0 to n - 1. Thus both the for loop and while loop has O(n) time complexity in total and the overall running time is O(n).
+
+There is another O(n) solution in this link, which is easier to understand and prove it is O(n). I have rewritten it below.
+*/
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
@@ -101,5 +109,47 @@ public:
             if (sum >= s) minlen = min(minlen, right - left);
         }
         return minlen == INT_MAX ? 0 : minlen;
+    }
+};
+
+// O(N*logN)
+/*
+Now let's move on to the O(nlogn) solution. Well, this less efficient solution is far more difficult to come up with. The idea is to first maintain an array of accumulated summations of elements in nums. Specifically, for nums = [2, 3, 1, 2, 4, 3] in the problem statement, sums = [0, 2, 5, 6, 8, 12, 15]. Then for each element in sums, if it is not less than s, we search for the first element that is greater than sums[i] - s (in fact, this is just what the upper_bound function does) in sums using binary search.
+
+Let's do an example. Suppose we reach 12 in sums, which is greater than s = 7. We then search for the first element in sums that is greater than sums[i] - s = 12 - 7 = 5 and we find 6. Then we know that the elements in nums that correspond to 6, 8, 12 sum to a number 12 - 5 = 7 which is not less than s = 7. Let's check for that: 6 in sums corresponds to 1 in nums, 8 in sums corresponds to 2 in nums, 12 in sums corresponds to 4 in nums. 1, 2, 4 sum to 7, which is 12 in sums minus 5 in sums.
+
+We add a 0 in the first position of sums to account for cases like nums = [3], s = 3.
+
+The code is as follows.
+*/
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        vector<int> sums = accumulate(nums);
+        int n = nums.size(), minlen = INT_MAX;
+        for (int i = 1; i <= n; i++) {
+            if (sums[i] >= s) {
+                int p = upper_bound(sums, 0, i, sums[i] - s);
+                if (p != -1) minlen = min(minlen, i - p + 1);
+            }
+        }
+        return minlen == INT_MAX ? 0 : minlen;
+    }
+private:
+    vector<int> accumulate(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sums(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+            sums[i] = nums[i - 1] + sums[i - 1];
+        return sums;
+    }
+    int upper_bound(vector<int>& sums, int left, int right, int target) {
+        int l = left, r = right;
+        while (l < r) {
+            int m = l + ((r - l) >> 1);
+            if (sums[m] <= target) l = m + 1;
+            else r = m;
+        }
+        return sums[r] > target ? r : -1;
     }
 };
