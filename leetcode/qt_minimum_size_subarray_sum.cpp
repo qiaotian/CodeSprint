97fa779e7aaf1498ba95d42d7607274bb1c6ec46
qiaotian <qiaotian@outlook.com>
2016-07-14 23:28:44 +0800

    update leetcode

--- leetcode/qt_minimum_size_subarray_sum.cpp
+++ leetcode/qt_minimum_size_subarray_sum.cpp
@@ -18,30 +18,88 @@ More practice:
 If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
 */
 
+2974,1094,6733,5177,4975,5421,8190,8255,9112,8651,2797,335,8677,3754,893,1818,8479,5875,1695,8295,7993,7037,8546,7906,4102,7279,1407,2462,4425,2148,2925,3903,5447,5893,3534,3663,8307,8679,8474,1202,3474,2961,1149,7451,4279,7875,5692,6186,8109,7763,7798,2250,2969,7974,9781,7741,4914,5446,1861,8914,2544,5683,8952,6745,4870,1848,7887,6448,7873,128,3281,794,1965,7036,8094,1211,9450,6981,4244,2418,8610,8681,2402,2904,7712,3252,5029,3004,5526,6965,8866,2764,600,631,9075,2631,3411,2737,2328,652,494,6556,9391,4517,8934,8892,4561,9331,1386,4636,9627,5435,9272,110,413,9706,5470,5008,1706,7045,9648,7505,6968,7509,3120,7869,6776,6434,7994,5441
 
+2974,1094,6733,5177,4975,5421,8190,8255,9112,8651,2797,335,8677,3754,893,1818,8479,5875,1695,8295,7993,7037,8546,7906,4102,7279,1407,2462,4425,2148,2925,3903,5447,5893,3534,3663,8307,8679,8679,8474,1202,3474,2961,1149,7451,4279,7875,5692,6186,8109,7763,7798,2250,2969,7974,9781,7741,4914,5446,1861,8914,2544,5683,8952,6745,4870,1848,7887,6448,7873,128,3281,794,1965,7036,8094,1211,9450,6981,4244,2418,8610,8681,2402,2904,7712,3252,5029,3004,5526,6965,8866,2764,600,631,9075,2631,3411,2737,2328,652,494,6556,9391,4517,8934,8892,4561,9331,1386,4636,9627,5435,9272,110,413,9706,5470,5008,1706,7045,9648,7505,6968,7509,3120,7869,6776,6434,7994,5441
+
+// beats 0.8%
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
-        if(nums.empty()) return -1;
-        if(s==1) return 1;
+        if(nums.empty()) return 0;
 
-        for(int i=1; i<nums.size(); i++) {
-            nums[i] += nums[i-1];
+        int ans = nums.size(), sum = 0, i = 0;
+        deque<int> dq;
+
+        while(i<nums.size()) {
+            sum += nums[i];
+            dq.push_back(nums[i]);
+            if(sum >= s) break;
+            i++;
         }
-        int window = 1;
-        while(nums[window-1] < s) window++; //
 
-        int sum = nums[window-1];
-        for(int i=0; i<nums.size()-window+1; i++) {
-            if(sum+nums[i+window]-nums[i]>=s) {
-                sum += nums[i+window]-nums[i];
-                while(sum-nums[i+1]>=s) {
-                    i++;
-                    window--;
-                }
-                i--;
+        if(sum<s && dq.size()==nums.size()) return 0;
+
+        while(sum-dq.front()>=s) {
+            sum-=dq.front();
+            dq.pop_front();
+            ans = dq.size();
+        }
+
+        cout << sum << endl;
+        cout << i << endl;
+        cout << dq.size() << endl;
+
+        // if(i>=nums.size()) return dq.size()
+        // 上面存在出队过程，所以dq的大小不是下一个未访问节点的下标
+        // 所以下一行 i=dq.size() 初始化错误
+        ans = min(ans, (int)dq.size());
+        for(/*i=dq.size()*/ i=i+1; i<nums.size(); i++) {
+            dq.push_back(nums[i]);
+            sum += (nums[i]-dq.front());
+            dq.pop_front();
+
+            cout << i << endl;
+            cout << dq.size() << endl;
+
+            while(sum-dq.front()>=s) {
+                sum-=dq.front();
+                dq.pop_front();
+                ans = min(ans, (int)dq.size());
             }
         }
-        return window;
+        return ans;
+    }
+};
+
+// O(N)
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int n = nums.size(), start = 0, sum = 0, minlen = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            while (sum >= s) {
+                minlen = min(minlen, i - start + 1);
+                sum -= nums[start++];
+            }
+        }
+        return minlen == INT_MAX ? 0 : minlen;
+    }
+};
+
+// O(N*logN)
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        int n = nums.size(), left = 0, right = 0, sum = 0, minlen = INT_MAX;
+        while (right < n) {
+            do sum += nums[right++];
+            while (right < n && sum < s);
+            while (left < right && sum - nums[left] >= s)
+                sum -= nums[left++];
+            if (sum >= s) minlen = min(minlen, right - left);
+        }
+        return minlen == INT_MAX ? 0 : minlen;
     }
 };
