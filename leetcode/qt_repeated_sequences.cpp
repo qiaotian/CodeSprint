40f2c64d15fd6105aec4c192b0b4d5f5239b005f
qiaotian <qiaotian@outlook.com>
2016-07-15 22:39:13 +0800

    update leetcode

--- leetcode/qt_repeated_sequences.cpp
+++ leetcode/qt_repeated_sequences.cpp
@@ -40,13 +40,13 @@ class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
         unordered_map<int, int> m;
-        vector<string> r;
+        vector<string> ret;
         int t = 0, i = 0, ss = s.size();
         while (i < 9)
             t = t << 3 | s[i++] & 7;
         while (i < ss)
             if (m[t = t << 3 & 0x3FFFFFFF | s[i++] & 7]++ == 1)
-                r.push_back(s.substr(i - 10, 10));
-        return r;
+                ret.push_back(s.substr(i - 10, 10));
+        return ret;
     }
 };
