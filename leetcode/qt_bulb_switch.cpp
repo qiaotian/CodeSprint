6d96c675191ef6e8ce428ef5a616cf5cc949bc6e
qiaotian <qiaotian@outlook.com>
2016-07-26 11:04:51 +0800

    update leetcode

--- leetcode/qt_bulb_switch.cpp
+++ leetcode/qt_bulb_switch.cpp
@@ -1,3 +1,25 @@
+/**
+* @Author: Tian Qiao
+* @Date:   2016-07-10T19:18:26+08:00
+* @Email:  qiaotian@me.com
+* @Last modified by:   Tian Qiao
+* @Last modified time: 2016-07-26T11:03:11+08:00
+*/
+
+/*
+There are n bulbs that are initially off. You first turn on all the bulbs. Then, you turn off every second bulb. On the third round, you toggle every third bulb (turning on if it's off or turning off if it's on). For the ith round, you toggle every i bulb. For the nth round, you only toggle the last bulb. Find how many bulbs are on after n rounds.
+
+Example:
+Given n = 3.
+
+At first, the three bulbs are [off, off, off].
+After first round, the three bulbs are [on, on, on].
+After second round, the three bulbs are [on, off, on].
+After third round, the three bulbs are [on, off, off].
+
+So you should return 1, because there is only one bulb is on.
+*/
+
 class Solution {
 public:
     int bulbSwitch(int n) {
@@ -12,17 +34,17 @@ public:
 /**
  * WARNING
  * Time: O(N^2) will lead to TLE
- * 
+ *
  * Tricky:
- * A light will be toggled only during the round of its factors, 
- * e.g. number 6 light will be toggled at 1,2,3,6 and light 12 
- * will be toggled at 1,2,3,4,6,12. The final state of a light 
- * is on and off only depends on if the number of its factor is 
- * odd or even. If odd, the light is on and if even the light is 
- * off. The number of one number's factor is odd if and only if 
- * it is a perfect square! So we will only need to loop to find 
+ * A light will be toggled only during the round of its factors,
+ * e.g. number 6 light will be toggled at 1,2,3,6 and light 12
+ * will be toggled at 1,2,3,4,6,12. The final state of a light
+ * is on and off only depends on if the number of its factor is
+ * odd or even. If odd, the light is on and if even the light is
+ * off. The number of one number's factor is odd if and only if
+ * it is a perfect square! So we will only need to loop to find
  * all the perfect squares that are smaller than n!
- * 
+ *
  * REFERNCE
  * https://leetcode.com/discuss/75808/my-0-ms-c-solution-with-explanation
  * /
---
+++ leetcode/qt_expression_add_operators.cpp
@@ -0,0 +1,28 @@
+/**
+* @Author: Tian Qiao
+* @Date:   2016-07-26T04:22:57+08:00
+* @Email:  qiaotian@me.com
+* @Last modified by:   Tian Qiao
+* @Last modified time: 2016-07-26T04:23:06+08:00
+* @Inc: Google, Facebook
+* @Difficulty: Hard
+*/
+
+
+/*
+Given a string that contains only digits 0-9 and a target value, return all possibilities to add binary operators (not unary) +, -, or * between the digits so they evaluate to the target value.
+
+Examples:
+"123", 6 -> ["1+2+3", "1*2*3"]
+"232", 8 -> ["2*3+2", "2+3*2"]
+"105", 5 -> ["1*0+5","10-5"]
+"00", 0 -> ["0+0", "0-0", "0*0"]
+"3456237490", 9191 -> []
+*/
+
+class Solution {
+public:
+    vector<string> addOperators(string num, int target) {
+        
+    }
+};
