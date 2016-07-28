8589659f92e30e56c6a29f01eda7be74a8126c69
qiaotian <qiaotian@outlook.com>
2016-07-28 10:19:35 +0800

    update leetcode

--- leetcode/qt_find_minimum_in_rotated_sorted_array.cpp
+++ leetcode/qt_find_minimum_in_rotated_sorted_array.cpp
@@ -1,3 +1,20 @@
+/**
+* @Author: Tian Qiao
+* @Date:   2016-07-10T19:18:26+08:00
+* @Email:  qiaotian@me.com
+* @Last modified by:   Tian Qiao
+* @Last modified time: 2016-07-28T00:58:57+08:00
+* @Inc: Microsoft
+* @Difficulty: Medium
+*/
+
+/*
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+Find the minimum element.
+You may assume no duplicate exists in the array.
+*/
+
 /*
 class Solution {
 public:
@@ -7,6 +24,8 @@ public:
     }
 };
 */
+
+// O(N)
 class Solution {
 public:
     int findMin(vector<int>& nums) {
@@ -15,5 +34,22 @@ public:
             if(nums[i] < nums[i-1]) return nums[i];
         }
         return res;
+    }
+};
+
+// O(logN) 二分查找
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size()-1;
+        while(l<r) {
+            // 因为l更新到m右边一位，因此有可能跳到递增区间上
+            if (nums[l]<nums[r]) return nums[l];
+
+            int m = (l+r)/2;
+            if(nums[m]>=nums[l]) l = m+1; // l肯定在min的左边
+            else if(nums[m]<nums[r]) r = m; // r在min的位置及右边
+        }
+        return nums[l];
     }
 };
---
+++ leetcode/qt_find_minimum_in_rotated_sorted_array_ii.cpp
@@ -0,0 +1,28 @@
+/**
+* @Author: Tian Qiao
+* @Date:   2016-07-28T00:55:21+08:00
+* @Email:  qiaotian@me.com
+* @Last modified by:   Tian Qiao
+* @Last modified time: 2016-07-28T00:55:24+08:00
+* @Inc: Unkown
+* @Difficulty: Hard
+*/
+
+
+/*
+Follow up for "Find Minimum in Rotated Sorted Array":
+What if duplicates are allowed?
+Would this affect the run-time complexity? How and why?
+
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+Find the minimum element.
+The array may contain duplicates.
+*/
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+    }
+};
