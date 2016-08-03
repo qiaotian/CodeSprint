b8c52407f7074de2ba63c7d7e40dcb1bb7b20426
qiaotian <qiaotian@outlook.com>
2016-08-03 19:46:06 +0800

    updates

--- leetcode/qt_closest_binary_search_tree_value.cpp
+++ leetcode/qt_closest_binary_search_tree_value.cpp
@@ -7,13 +7,13 @@
 * @Inc: Google+MS+Snapchat
 */
 
+/*
 Given a non-empty binary search tree and a target value, find the value in the BST that is closest to the target.
 
 Note:
 Given target value is a floating point.
 You are guaranteed to have only one unique value in the BST that is closest to the target.
-
-________________________________________________________________________________
+*/
 
 /**
  * Definition for a binary tree node.
