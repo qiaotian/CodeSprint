1d3c96b9f4f1fce20caa68a7eac2a6de925f3fea
qiaotian <qiaotian@outlook.com>
2016-08-07 23:53:14 +0800

    updates

--- leetcode/qt_find_bad_versions.cpp
+++ leetcode/qt_find_bad_versions.cpp
@@ -16,3 +16,24 @@ Suppose you have n versions [1, 2, ..., n] and you want to find out the first ba
 
 You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
 */
+
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n, mid;
+        while(left<right) {
+            mid = (int)(((long)left+(long)right)/2);
+            if(isBadVersion(mid)) right = mid;
+            else left = mid+1;
+        }
+        return left;
+    }
+};
+
+/***
+ * Pay attention to the range integer could represent, which
+ * means (left+right) could be out of range of int
+ * /
