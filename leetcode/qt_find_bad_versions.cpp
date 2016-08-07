5a8238938fd8578332626e26da7c64e1d9d40377
qiaotian <qiaotian@outlook.com>
2016-08-07 23:53:02 +0800

    updates

---
+++ leetcode/qt_find_bad_versions.cpp
@@ -0,0 +1,18 @@
+/**
+* @Author: Tian Qiao
+* @Date:   2016-08-07T23:52:26+08:00
+* @Email:  qiaotian@me.com
+* @Last modified by:   Tian Qiao
+* @Last modified time: 2016-08-07T23:52:27+08:00
+* @Tag: Binary Search
+*/
+
+
+
+/*
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+*/
