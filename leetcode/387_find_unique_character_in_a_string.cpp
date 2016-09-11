de40c2f9e467562b04da60133bb46eaa3496fc77
qiaotian <qiaotian@outlook.com>
2016-09-11 11:28:51 +0800

    hm

---
+++ leetcode/387_find_unique_character_in_a_string.cpp
@@ -0,0 +1,58 @@
+/*
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+*/
+
+
+// T: O(2N) S: O(N)
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int ans = -1;
+        if(s.empty()) return ans;
+        
+        vector<int> dp(26, 0);
+        
+        int n = s.size();
+        for(int i=0; i<n; i++) {
+            dp[s[i]-'a']++;
+        }
+        for(int i=0; i<n; i++) {
+            if(dp[s[i]-'a']==1) return i;
+        }
+        return ans;
+    }
+};
+
+
+// T: O(2N) S: O(1)
+class Solution {
+public:
+    int firstUniqChar(string s) {
+		int one = 0, two = 0;
+        for(auto c:s) {
+            bit = 1 << ord(char) - 97
+            if not one & bit:
+                if not two & bit:
+                    one |= bit
+                continue
+
+            one &= ~bit
+            two |= bit
+		}
+        for i, char in enumerate(s):
+            bit = 1 << ord(char) - 97
+            if one & bit:
+                return i
+
+        return -1
+	}
+}
