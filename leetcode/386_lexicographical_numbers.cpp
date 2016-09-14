4e7c5c173b786901491aa81efce710949625c3c7
Tian Qiao <qiaotian@outlook.com>
2016-09-14 22:25:09 +0800

    Add a new method

--- leetcode/386_lexicographical_numbers.cpp
+++ leetcode/386_lexicographical_numbers.cpp
@@ -42,3 +42,27 @@ public:
         }
     }
 };
+
+
+// Faster Method with no recursion
+// https://discuss.leetcode.com/topic/55131/ac-240ms-c-solution
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> res(n);
+        int cur = 1;
+        for (int i = 0; i < n; i++) {
+            res[i] = cur;
+            if (cur * 10 <= n) {
+                cur *= 10;
+            } else {
+                if (cur >= n) 
+                    cur /= 10;
+                cur += 1;
+                while (cur % 10 == 0)
+                    cur /= 10;
+            }
+        }
+        return res;
+    }
+};
