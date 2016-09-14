25361d80af8d37d5b5d416e27c764fca2aa0ca2e
Tian Qiao <qiaotian@outlook.com>
2016-09-14 22:20:42 +0800

    Create 386_lexicographical_numbers.cpp

---
+++ leetcode/386_lexicographical_numbers.cpp
@@ -0,0 +1,44 @@
+/*
+Given an integer n, return [1, n] in lexicographical order.
+For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
+Please optimize your algorithm to use less time and space. The input size may be as large as 5,000,000. 
+*/
+
+// TLE
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        vector<string> strs;
+        for(int i=1; i<n+1; i++) {
+            string tmp = to_string(i);
+            strs.push_back(tmp);
+        }
+        sort(strs.begin(), strs.end());
+        for(auto s:strs) {
+            int tmp = stoi(s);
+            ans.push_back(tmp);
+        }
+        return ans;
+    }
+};
+
+
+// DFS
+// 1->10->100->1000->...->10001->10002->...2->20->...
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        for(int i=1; i<10; i++)
+            util(ans, i, n); //收集以1,2,3...,i,...9开头的数字
+        return ans;
+    }
+    void util(vector<int>& ans, int cur, int& bound) {
+        if(cur > bound) return;
+        ans.push_back(cur);
+        for(int i=0; i<10; i++) {
+            util(ans, cur*10+i, bound);
+        }
+    }
+};
