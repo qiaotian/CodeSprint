c3fa04cb22af3e3c4d8b951518a190e54fa8c121
qiaotian <qiaotian@outlook.com>
2016-09-22 03:40:25 +0800

    update min stack

--- leetcode/155_min_stack.cpp
+++ leetcode/155_min_stack.cpp
@@ -71,4 +71,34 @@ public:
 /**
  * All these method will use two containers 
  * including one for minimal value
- * /
+ */
+
+// 改进：将空间复杂度降到O(1)
+// The current min among the stack elements.
+// For each pushed element, the difference to the previous min. If the pushed element changes min, because it is even smaller, then this difference is negative. If it's larger than the previous min, then the difference is positive.
+// For pop, I need to pop the last difference and undo its change to min (if any). For top, I need to add the current min and the last difference (if it's positive, i.e., if the last element was larger than the minimum).
+
+class MinStack {
+private:
+    long long minval;
+    vector<long long> vec;
+public:
+    MinStack() {
+        minval = INT32_MAX;
+    }
+    void push(int x) {
+        vec.push_back((long long)x-minval); // x极小且minmal极小，很容易溢出
+        minval = min((long long)x, minval);
+    }
+    void pop() {
+        long long tmp = vec.back();
+        minval -= min(tmp, (long long)0); 
+        vec.pop_back();
+    }
+    int top() {
+        return max(vec.back(), (long long)0)+minval;
+    }
+    int getMin(){
+        return minval;
+    }
+};
