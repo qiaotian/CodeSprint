0714dba361126a86937702ca97c709f7586069eb
Tian Qiao <qiaotian@outlook.com>
2017-11-06 01:11:04 +0800

    modify two sum

--- leetcode/cpp/001_two_sum.cpp
+++ leetcode/cpp/001_two_sum.cpp
@@ -11,10 +11,17 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> dic;
+        vector<int> ans;
         for (int i = 0; i < nums.size(); i++) {
-            if (dic[target - nums[i]]) return vector<int> ans(nums[i], target - nums[i]);
-            dic[target - nums[i]] = nums[i];
+            if ( dic.find(target - nums[i]) != dic.end() ) {
+                ans.push_back(i);
+                ans.push_back(dic[target - nums[i]]);
+                return ans;
+            }
+            dic[nums[i]] = i;
         }
-        return return vector<int> ans(-1, -1);
+        ans.push_back(-1);
+        ans.push_back(-1);
+        return ans;
     }
 };
